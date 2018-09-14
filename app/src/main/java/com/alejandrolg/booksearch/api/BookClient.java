213a995ca40607e8a7b1c8865bfeedfff0347573
Alejandro Loza <al02812273@tecmilenio.com>
2018-09-13 20:29:50 -0500

    Initial commit

---
+++ app/src/main/java/com/alejandrolg/booksearch/api/BookClient.java
@@ -0,0 +1,44 @@
+package com.alejandrolg.booksearch.api;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.alejandrolg.booksearch.models.Book;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+public class BookClient {
+    private static final String API_BASE_URL = "http://openlibrary.org/";
+    private AsyncHttpClient client;
+    public void getExtraBookDetails(String openLibraryId, JsonHttpResponseHandler handler) {
+        String url = getApiUrl("books/");
+        client.get(url + openLibraryId + ".json", handler);
+    }
+
+    public BookClient() {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // Method for accessing the search API
+    public void getBooks(final String query, JsonHttpResponseHandler handler) {
+        try {
+            String url = getApiUrl("search.json?q=");
+            client.get(url + URLEncoder.encode(query, "utf-8"), handler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
